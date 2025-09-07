497a3c4a8c427196b2fdd492fdecd45d194e2e96
Tony Ward <tonygward@googlemail.com>
2025-09-07 14:50:34 +0100

    added psfdx-logs

--- install-linux.ps1
+++ install-linux.ps1
@@ -1,2 +1,5 @@
 sudo rm -rf /opt/microsoft/powershell/7/Modules/psfdx
 sudo cp -rf psfdx /opt/microsoft/powershell/7/Modules/
+
+sudo rm -rf /opt/microsoft/powershell/7/Modules/psfdx-logs
+sudo cp -rf psfdx-logs /opt/microsoft/powershell/7/Modules/
--- install-windows.ps1
+++ install-windows.ps1
@@ -1,2 +1,5 @@
 Remove-Item $Home\Documents\PowerShell\Modules\psfdx -Recurse -Force
 Copy-Item psfdx $Home\Documents\PowerShell\Modules -Recurse -Force
+
+Remove-Item $Home\Documents\PowerShell\Modules\psfdx-logs -Recurse -Force
+Copy-Item psfdx-logs $Home\Documents\PowerShell\Modules -Recurse -Force
