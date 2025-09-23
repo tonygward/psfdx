9cbd65cda6626661680d14b724b8750c974153a0
Tony Ward <tonygward@googlemail.com>
2025-09-23 22:36:15 +0100

    minor refactor of Invoke-Salesforce.ps1

--- psfdx-shared/Invoke-Salesforce.ps1
+++ psfdx-shared/Invoke-Salesforce.ps1
@@ -1,22 +1,9 @@
 function Invoke-Salesforce {
     [CmdletBinding()]
     Param(
-        [Parameter(Mandatory = $true)][object] $Command
+        [Parameter(Mandatory = $true)][string] $Command
     )
 
-    if ($Command -is [string[]]) {
-        Write-Verbose ($Command -join ' ')
-        if ($Command.Length -eq 0) { throw 'No command specified' }
-        $exe = $Command[0]
-        $args = @()
-        if ($Command.Length -gt 1) { $args = $Command[1..($Command.Length-1)] }
-        return & $exe @args
-    }
-    elseif ($Command -is [string]) {
-        Write-Verbose $Command
-        return Invoke-Expression -Command $Command
-    }
-    else {
-        throw "Unsupported -Command type: $($Command.GetType().FullName)"
-    }
+    Write-Verbose $Command
+    return Invoke-Expression -Command $Command
 }
