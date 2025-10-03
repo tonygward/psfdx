3dc58a50339825423c489a026b49d72f8fffae18
Tony Ward <tonygward@googlemail.com>
2025-10-03 19:50:53 +0100

    refactored

--- psfdx-shared/SalesforceApexTests.ps1
+++ psfdx-shared/SalesforceApexTests.ps1
@@ -5,22 +5,63 @@ function Get-SalesforceApexCliTestParams {
         [Parameter(Mandatory = $false)][ValidateSet(
             'NoTests',
             'SpecificTests',
+            'TestsClass',
             'TestsInFolder',
             'TestsInOrg',
             'TestsInOrgAndPackages')][string] $TestLevel = 'NoTests',
         [Parameter(Mandatory = $false)][string[]] $Tests
     )
 
+    $value = ""
     $testLevelMap = @{
         'NoTests'               = 'NoTestRun'
         'SpecificTests'         = 'RunSpecifiedTests'
+        'TestsClass'            = 'RunSpecifiedTests'
         'TestsInFolder'         = 'RunSpecifiedTests'
         'TestsInOrg'            = 'RunLocalTests'
         'TestsInOrgAndPackages' = 'RunAllTestsInOrg'
     }
     $value += " --test-level " + $testLevelMap[$TestLevel]
 
-    if ($TestLevel -eq 'TestsInFolder') {
+    if ($TestLevel -eq 'TestsClass') {
+        if (-not $SourceDir) {
+            throw "Specify -SourceDir when using -TestLevel TestsClass."
+        }
+        if (-not (Test-Path -LiteralPath $SourceDir)) {
+            throw "Source path '$SourceDir' does not exist."
+        }
+
+        $item = Get-Item -LiteralPath $SourceDir
+        if ($item.PSIsContainer) {
+            throw "Provide a file path for -SourceDir when using -TestLevel TestsClass."
+        }
+
+        $className = [System.IO.Path]::GetFileNameWithoutExtension($item.Name)
+        if (-not $className) {
+            throw "Unable to determine class name from '$SourceDir'."
+        }
+
+        $searchRoot = $item.Directory
+        if (-not $searchRoot) {
+            throw "Unable to determine directory for '$SourceDir'."
+        }
+
+        $escapedClassName = [regex]::Escape($className)
+        $classPattern = "\b$escapedClassName\b"
+
+        $Tests = Get-ChildItem -LiteralPath $searchRoot.FullName -Filter '*.cls' -File -ErrorAction SilentlyContinue |
+            Where-Object {
+                $_.FullName -ne $item.FullName -and
+                (Select-String -Path $_.FullName -Pattern '@isTest' -SimpleMatch -Quiet) -and
+                (Select-String -Path $_.FullName -Pattern $classPattern -Quiet)
+            } |
+            ForEach-Object { $_.BaseName } |
+            Sort-Object -Unique
+
+        if (-not $Tests -or $Tests.Count -eq 0) {
+            throw "No Apex test classes in '$($searchRoot.FullName)' reference '$className'."
+        }
+    } elseif ($TestLevel -eq 'TestsInFolder') {
         $TestsPath = $SourceDir
         if (-not $TestsPath) {
             $TestsPath = Get-Location
